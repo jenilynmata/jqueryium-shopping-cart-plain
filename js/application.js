53135135c435aaf99e729ce4e3b52f316b93780d
jenilynmata <92706137+jenilynmata@users.noreply.github.com>
2024-03-30 16:27:26 -0700

    Added more items and made title bold

--- js/application.js
+++ js/application.js
@@ -37,4 +37,24 @@ $(document).ready(function () {
             updateTotal();
         }, 1000);
     });
-});
+
+    $('#addItem').on('submit', function(event) {
+        event.preventDefault();
+        var name = $(this).children('[name=name]').val();
+        var price = $(this).children('[name=price]').val();
+        var qty = $(this).children('[name=qty]').val();
+        
+        $('tbody').append('<tr>' +
+        '<td class="name">' + name + '</td>' + 
+        '<td class="price">' + price + '</td>' + 
+        '<td><input class="qty" name="qty" type="number" value="' + qty + '" /></td>' + 
+        '<td><button class="btn btn-light btn-sm remove">remove</button></td>' + 
+        '<td class="totalPrice">$-.--</td>'
+        );
+
+        updateTotal();
+        $(this).children('[name=name]').val('');
+        $(this).children('[name=price]').val('');
+        $(this).children('[name=qty]').val('');
+    });
+});    
